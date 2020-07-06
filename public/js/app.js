22e79e943cae0857112705ecd1603fd98ebbc274
sohamGit123 <sohamsarkar916@gmail.com>
2020-07-07 03:56:10 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -19,7 +19,7 @@ weatherForm.addEventListener('submit',(e)=>{
 
     const location=searchElement.value
 
-    const url='http://localhost:3000/weather?address=' + location
+    const url='/weather?address=' + location
     messageOne.textContent='Loading...'
     messageTwo.textContent=''
 
